26891f81747c738733db797936c28c7bf5714a41
Christian Kühnel <christian.kuehnel@gmail.com>
2018-04-21 18:45:50 +0200

    merged previous changes to latest implementation

--- homeassistant/components/cover/zwave.py
+++ homeassistant/components/cover/zwave.py
@@ -20,13 +20,32 @@ _LOGGER = logging.getLogger(__name__)
 SUPPORT_GARAGE = SUPPORT_OPEN | SUPPORT_CLOSE
 
 
+def _to_hex_str(id_in_bytes):
+    """convert a two byte value to a hex string.
+
+     Example: 0x1234 --> '0x1234' """
+    return '0x{:04x}'.format(id_in_bytes)
+
+# For whatever reason node.manufacturer_id is of type string. So we need
+# to convert the values.
+FIBARO = _to_hex_str(workaround.FIBARO)
+FIBARO_SHUTTERS = [_to_hex_str(workaround.FGR222_SHUTTER2),
+                   _to_hex_str(workaround.FGRM222_SHUTTER2)]
+
+
 def get_device(hass, values, node_config, **kwargs):
     """Create Z-Wave entity device."""
     invert_buttons = node_config.get(zwave.CONF_INVERT_OPENCLOSE_BUTTONS)
     if (values.primary.command_class ==
             zwave.const.COMMAND_CLASS_SWITCH_MULTILEVEL
             and values.primary.index == 0):
-        return ZwaveRollershutter(hass, values, invert_buttons)
+        if values.primary.node.manufacturer_id == FIBARO \
+                and values.primary.node.product_type in FIBARO_SHUTTERS:
+            # TODO: also check for venetian blind mode
+            # need to read the ZWave configuration of the device for that
+            return FibaroFGM222(hass, values, invert_buttons)
+        else:
+            return ZwaveRollershutter(hass, values, invert_buttons)
     elif (values.primary.command_class ==
           zwave.const.COMMAND_CLASS_SWITCH_BINARY):
         return ZwaveGarageDoorSwitch(values)
@@ -174,3 +193,77 @@ class ZwaveGarageDoorBarrier(ZwaveGarageDoorBase):
     def open_cover(self, **kwargs):
         """Open the garage door."""
         self.values.primary.data = "Opened"
+
+
+class FibaroFGM222(ZwaveRollershutter):
+    """Implementation of proprietary features for Fibaro FGR-222 / FGRM-222.
+
+    This adds support for the tile feature for the ventian blind mode.
+    For this to work, you need to set the Zwave device configuration.
+    """
+    # TODO: describe the required configuration parameters
+
+    def __init__(self, hass, values, invert_buttons):
+        """Initialize the FFM222 with tilt mode enabled"""
+        self._cached_value_blinds = None
+        self._cached_value_tilt = None
+        super().__init__(hass, values, invert_buttons)
+        _LOGGER.debug('Device is a Fibaro FGR-222/FGRM-222 with tilt mode.')
+
+    @property
+    def current_cover_tilt_position(self):
+        """Get the tilt of the bilnds."""
+        if self._value_tilt is None:
+            return None
+        return self._value_tilt.data
+
+    def set_cover_tilt_position(self, tilt_position, **kwargs):
+        """Move the cover tilt to a specific position."""
+        _LOGGER.debug("setting tilt to %d", tilt_position)
+        if self._value_tilt is not None:
+            self._value_tilt.data = tilt_position
+
+    def open_cover_tilt(self, **kwargs):
+        """Set slats to horizontal position."""
+        self.set_cover_tilt_position(50)
+
+    def close_cover_tilt(self, **kwargs):
+        """Close the slats."""
+        self.set_cover_tilt_position(0)
+
+    @property
+    def _value_blinds(self):
+        """Read the blind position."""
+        if self._cached_value_blinds is None:
+            self._find_values()
+        return self._cached_value_blinds
+
+    @property
+    def _value_tilt(self):
+        """Read the tilt position."""
+        if self._cached_value_tilt is None:
+            self._find_values()
+        return self._cached_value_tilt
+
+    def set_cover_position(self, position, **kwargs):
+        """Move the roller shutter to a specific position."""
+        if self._value_blinds is not None:
+            self._value_blinds.data = position
+
+    def _find_values(self):
+        """Get the current position from the zwave library."""
+        values = self.node.get_values(class_id=0x91)
+        for _, value in values.items():
+            if value.index == 0:
+                self._cached_value_blinds = value
+            elif value.index == 1:
+                self._cached_value_tilt = value
+            else:
+                _LOGGER.warning('Undefined index %d for this command class',
+                                value.index)
+
+    def update_properties(self):
+        """React on properties being updated."""
+        super().update_properties()
+        if self._value_blinds is not None:
+            self._current_position = self._value_blinds.data
